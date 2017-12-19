5de661bdb4e62ce582b8b45343f45d7fcb66af57
zvermandr <zvermandr@gmail.com>
2017-12-19 20:01:54 +0300

    Complete Web version of Cryptography Notepad

--- src/main/java/mandrik/security/notepad/CryptographySecurityNotepadApplication.java
+++ src/main/java/mandrik/security/notepad/CryptographySecurityNotepadApplication.java
@@ -2,8 +2,10 @@ package mandrik.security.notepad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CryptographySecurityNotepadApplication {
 
 	public static void main(String[] args) {
--- src/main/java/mandrik/security/notepad/controller/NotepadRestController.java
+++ src/main/java/mandrik/security/notepad/controller/NotepadRestController.java
@@ -2,14 +2,17 @@ package mandrik.security.notepad.controller;
 
 
 
-import mandrik.security.notepad.controller.utils.RestUtils;
 import mandrik.security.notepad.controller.utils.ValuesConfiguration;
 import mandrik.security.notepad.service.crypto.FileCipher;
+import org.apache.commons.io.IOUtils;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.*;
 
 
 import javax.crypto.BadPaddingException;
@@ -17,14 +20,13 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
+import java.nio.file.Paths;
 import java.security.*;
-import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Arrays;
-import java.util.Base64;
 import java.util.Map;
 import java.util.Random;
 
@@ -58,20 +60,20 @@ public class NotepadRestController {
         session.setAttribute("sessionKey", key);
 
 
-        Map response = RestUtils.mapOf("sessionKey", sessionKey);
         LOGGER.info("Generated new session key: " + Arrays.toString(sessionKey));
         return "success";
     }
 
     @PostMapping("/download")
-    public Map downloadFile(@RequestBody Map<String, String> mapFileName, HttpServletRequest request) {
+    public void downloadFile(@RequestBody Map<String, String> mapFileName,
+                            HttpServletRequest request, HttpServletResponse response) {
 
         String fileName = mapFileName.get("fileName");
 
         String downloadFilePath = valuesConfiguration.getFilesDir() + fileName;
         String encryptFilePath = valuesConfiguration.getEncryptFilesDir() + fileName;
         FileCipher fileCipher = new FileCipher(downloadFilePath, encryptFilePath,
-                String.valueOf(request.getSession().getAttribute("sessionKey")), true);
+                Base64.encodeBase64String(toByteArray((Byte[])request.getSession().getAttribute("sessionKey"))), true);
 
         try {
             fileCipher.cryptFile();
@@ -79,67 +81,109 @@ public class NotepadRestController {
             e.printStackTrace();
         }
 
-        fileCipher = new FileCipher(encryptFilePath, valuesConfiguration.getFilesDir() + "123.txt",
-                String.valueOf(request.getSession().getAttribute("sessionKey")), false);
+        Paths.get(encryptFilePath);
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(encryptFilePath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            IOUtils.copy(inputStream, response.getOutputStream());
+            response.flushBuffer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /*fileCipher = new FileCipher(encryptFilePath, valuesConfiguration.getFilesDir() + "temp.txt",
+                Base64.encodeBase64String(toByteArray((Byte[])request.getSession().getAttribute("sessionKey"))), false);
 
         try {
             fileCipher.cryptFile();
         } catch (IOException e) {
             e.printStackTrace();
-        }
-
-        return RESULT_SUCCESS;
+        }*/
     }
 
 
     @PostMapping("/upload/public-rsa-key")
     public Map uploadOpenRSAKey(@RequestBody Map<String, String> openRSAKey, HttpServletRequest request) {
 
-
         if(!openRSAKey.containsKey("key")) {
             return RESULT_ERROR;
         }
+        String publicK = openRSAKey.get("key");
+        byte[] publicBytes = Base64.decodeBase64(publicK);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicBytes);
+        PublicKey pubKey;
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            pubKey = keyFactory.generatePublic(keySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+            return RESULT_ERROR;
+        }
 
-        String rsaKey = openRSAKey.get("key");
-        byte[] openKey = Base64.getDecoder().decode(rsaKey);
+        LOGGER.info(pubKey.toString());
 
-        LOGGER.info(Arrays.toString(openKey));
-
-        Byte[] key = new Byte[valuesConfiguration.getSessionKeyLength()];
-        int index = 0;
-        for(byte b: openKey)
-            key[index++] = b;
         HttpSession session = request.getSession();
-        session.setAttribute("publicRSAKey", key);
+        session.setAttribute("publicRSAKey", pubKey);
 
         return RESULT_SUCCESS;
     }
 
 
-    @GetMapping("/key/get")
+    @GetMapping("/session-key/get")
     public Map getSessionKeyEncryptRSA(HttpServletRequest request) {
 
         HttpSession session = request.getSession();
         Byte[] sessionKey = (Byte[]) session.getAttribute("sessionKey");
-        Byte[] publicRSAKey = (Byte[]) session.getAttribute("publicRSAKey");
+        PublicKey publicRSAKey = (PublicKey) session.getAttribute("publicRSAKey");
 
+        Cipher cipher;
 
-        byte[] encryptSessionKey = new byte[0];
         try {
-            Cipher cipher = Cipher.getInstance("RSA");
-            byte[] rsaKey = new byte[publicRSAKey.length];
-
-            X509EncodedKeySpec spec = new X509EncodedKeySpec(rsaKey);
-            KeyFactory fact = KeyFactory.getInstance("RSA");
-
-            cipher.init(Cipher.ENCRYPT_MODE, fact.generatePublic(spec));
-
-            encryptSessionKey = cipher.doFinal();
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeySpecException
-                | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            cipher = Cipher.getInstance("RSA");
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            e.printStackTrace();
             return RESULT_ERROR;
         }
 
-        return mapOf("sessionKey", sessionKey);
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, publicRSAKey);
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+            return RESULT_ERROR;
+        }
+
+        byte[] encryptSessionKey;
+        try {
+            encryptSessionKey = cipher.doFinal(toByteArray(sessionKey));
+        } catch (IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+            return RESULT_ERROR;
+        }
+
+        String sessionKeyString = Base64.encodeBase64String(encryptSessionKey);
+
+        return mapOf("sessionKey", sessionKeyString);
+    }
+
+    public static byte[] toByteArray(Byte[] byteArray) {
+        byte[] bytes = new byte[byteArray.length];
+
+        for(int i = 0; i < byteArray.length; i++) {
+            bytes[i] = byteArray[i];
+        }
+        return bytes;
+    }
+
+    public static Byte[] toByteObjectArray(byte[] bytes) {
+        Byte[] byteArray = new Byte[bytes.length];
+
+        for(int i = 0; i < bytes.length; i++) {
+            byteArray[i] = bytes[i];
+        }
+        return byteArray;
     }
 }
